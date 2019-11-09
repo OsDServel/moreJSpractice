a91c00e73a80e8204ca9b10ebd112f60e8fe0a91
Oscar Servellon <rockerolife66@gmail.com>
2019-11-09 00:09:17 -0800

    first try at practicing javascript class exercises

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//  for(var i= 0; i<= 100; i++){
+// 	// console.log(i);
+// 	console.log(i);
+// }
+
+// for(i= 2; i<= 100; i++){
+// 	console.log(i);
+// }
+
+for(i= 2; i<= 100; i+ 2){
+	console.log(i);
+}
